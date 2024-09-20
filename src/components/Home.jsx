c1be6a5338b1215c9d083090bc654cdb4db152f5
rodrigoMRAM <rodrigomacielth@gmail.com>
2024-09-20 19:40:36 -0300

    agregue google font

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import imagen from '../img/code.jpeg'
+import imagenMain from '../img/main.png'
 export const Home = () => {
   return (
     <>
     <div className="w-full lava ">
     <div >
-       <img className="h-[500px] w-full" src={imagen} alt="" srcset="" />
+       <img className="h-[750px] w-full" src={imagenMain} alt="" srcset="" />
     </div>
-       <div className="flex flex-col justify-center items-center absolute top-32 w-full h-fit mt-10">
-           <div className="flex flex-col justify-center items-center">
+       <div className="flex flex-col justify-center items-center absolute top-44 w-full h-fit mt-10">
+           <div className="flex flex-col justify-center items-center text-center">
                
-               <h2 className="text-4xl mt-5 text-white flex">Create and grow your
-                   unique website today</h2>
-                   <p className="text-center mt-5 text-white">Programmatically work but low hanging fruit so new economy cross-pollination. <br/>Quick sync new
+               <h2 className="text-5xl font-bold mt-5 text-white flex">Software is a collection of <br /> programs and data.</h2>
+                   <p className="text-center mt-5 text-white leading-6">Programmatically work but low hanging fruit so new economy cross-pollination. <br/>Quick sync new
                     economy onward and upward.</p>
                 </div>
                 <div className="mt-5"><button className="rounded button py-2 px-2 text-white font-medium transition delay-100 hover:bg-white hover:text-black">Learn More</button>
@@ -22,17 +22,19 @@ export const Home = () => {
             </div>
             <div className="services">
 
-            <div className="mt-14 flex items-center justify-center w-full">
+            <div className="mt-24 flex items-center justify-center w-full">
                 <div className="flex w-4/6 items-center flex-wrap">
-                    <div className="flex flex-col text-center items-center w-[400px] flotante h-[250px]">
+                    <div className="flex flex-col text-center items-center w-[400px] flotante h-[320px] cursor-pointer">
 
-                        <div>
-                            
-                            <img src="krakio.jpeg" alt="" width="90px"/>
+                   
+                        <div className='flex flex-col py-10'>
+
+                        <div className=''>
+                            <span class="material-symbols-outlined p-2 text-white text-5xl bg-[#5580FF] border-2 border-[#5580FF] rounded-full hover:bg-white hover:text-[#5580FF] duration-200">
+                            rewarded_ads
+                            </span>
                         </div>
-                        <div>
-                            <img src="" alt="" />
-                            <h5 className="mt-10 font-bold">
+                            <h5 className="mt-5 font-bold">
                                 Cognitive Automation
                             </h5>
                             <p>
@@ -40,14 +42,14 @@ export const Home = () => {
                             </p>
                         </div>
                     </div>
-                    <div className="flex flex-col text-center items-center w-[400px] border flotante h-[250px]">
+                    <div className="flex flex-col text-center items-center w-[400px] ml-4 mr-4 border flotante h-[320px] cursor-pointer">
 
-                        <div>
-                            
-                            <img src="krakio.jpeg" alt="" width="90px"/>
-                        </div>
-                        <div>
-                            <h5 className="mt-10 font-bold">
+                  
+                        <div className='py-10'>
+                            <span class="material-symbols-outlined p-2 text-white text-5xl bg-[#5580FF] border-2 border-[#5580FF] rounded-full hover:bg-white hover:text-[#5580FF] duration-200">
+social_leaderboard
+</span>
+                            <h5 className="mt-5 font-bold">
                                 Cognitive Automation
                             </h5>
                             <p>
@@ -55,15 +57,14 @@ export const Home = () => {
                             </p>
                         </div>
                     </div>
-                    <div className="flex flex-col text-center items-center w-[400px] flotante h-[250px]">
+                    <div className="flex flex-col text-center items-center w-[400px]  flotante h-[320px] cursor-pointer">
 
-                        <div>
-                            
-                            
-                            <img src="krakio.jpeg" alt="" width="90px"/>
-                        </div>
-                        <div>
-                            <h5 className="mt-10 font-bold">
+        
+                        <div className='py-10'>
+                            <span class="material-symbols-outlined p-2 text-white text-5xl bg-[#5580FF] border-2 border-[#5580FF] rounded-full hover:bg-white hover:text-[#5580FF] duration-200">
+partner_exchange
+</span>
+                            <h5 className="mt-5 font-bold">
                                 Cognitive Automation
                             </h5>
                             <p>
@@ -75,19 +76,19 @@ export const Home = () => {
                 </div>
 
             </div>
-            <div className="ourVision flex w-full mt-14">
-                <div className='flex flex-col justify-center items-center w-full mt-14 mb-14'>
-                <div><h2 className='font-bold'>Our Mission & Vision</h2></div>
+            <div className="ourVision flex w-full mt-24">
+                <div className='flex flex-col justify-center items-center w-full mt-24 mb-24'>
+                <div className='mb-10'><h2 className='font-bold text-3xl'>Our Mission & Vision</h2></div>
                 <div><p>Standards compliant e-business.Phosfluorescently expedite functional products via premium action items wireless innovation compliant e-business.</p></div>
-                <div className='flex flex-col'>
+                <div className='flex flex-col w-3/5'>
                     <div className='division1 flex'>
 
-                    <div className='h-[250px] w-72 border-blue rounded flex items-center'>
-                        <div className='flex'>
+                    <div className='h-[250px] w-2/5 border-blue rounded flex items-center'>
+                        <div className='flex '>
 
                         <div>
 
-                    <span class="material-symbols-outlined">
+                    <span class="material-symbols-outlined p-2 text-[#5580FF] text-5xl border rounded-full">
 select_check_box
 </span>
                         </div>
@@ -99,12 +100,12 @@ select_check_box
                         </div>
                     </div>
                         </div>
-                    <div className='h-[250px] w-72 border-blue rounded flex items-center'>
+                    <div className='h-[250px] w-2/5 border-blue rounded flex items-center'>
                         <div className="flex">
 
                         <div>
 
-                    <span class="material-symbols-outlined">
+                    <span class="material-symbols-outlined p-2 text-[#5580FF] text-5xl border rounded-full">
 cardiology
 </span>
                         </div>
@@ -116,12 +117,12 @@ cardiology
                         </div>
                     </div>
                         </div>
-                    <div className='h-[250px] w-72 border-blue rounded flex items-center'>
+                    <div className='h-[250px] w-2/5 border-blue rounded flex items-center'>
                         <div className='flex'>
 
                         <div>
 
-                    <span class="material-symbols-outlined">
+                    <span class="material-symbols-outlined p-2 text-[#5580FF] text-5xl border rounded-full">
 apartment
 </span>
                         </div>
@@ -134,12 +135,12 @@ apartment
                         </div>
                     <div className='division2 flex'>
 
-                    <div className='h-[250px] w-72 border-blue rounded flex items-center'>
+                    <div className='h-[250px] w-2/5 border-blue rounded flex items-center'>
                         <div className="flex">
 
                         <div>
 
-                    <span class="material-symbols-outlined">
+                    <span class="material-symbols-outlined p-2 text-[#5580FF] text-5xl border rounded-full">
 monitoring
 </span>
                         </div>
@@ -150,12 +151,12 @@ monitoring
                         </div>
                     </div>
                         </div>
-                    <div className='h-[250px] w-72 border-blue rounded flex items-center'>
+                    <div className='h-[250px] w-2/5 border-blue rounded flex items-center'>
                         <div className="flex">
 
                         <div>
 
-                    <span class="material-symbols-outlined">
+                    <span class="material-symbols-outlined p-2 text-[#5580FF] text-5xl border rounded-full">
 support_agent
 </span>
                         </div>
@@ -165,12 +166,12 @@ support_agent
                     <p>Progress moves us forward without marginalizing efforts to disrupt responsibility toward a potential the compromising.</p></div>
                         </div>
                         </div>
-                    <div className='h-[250px] w-72 border-blue rounded flex items-center'>
+                    <div className='h-[250px] w-2/5 border-blue rounded flex items-center'>
                         <div className="flex">
 
                         <div>
 
-                    <span class="material-symbols-outlined">
+                    <span class="material-symbols-outlined p-2 text-[#5580FF] text-5xl border rounded-full">
 contract
 </span>
                         </div>
@@ -228,7 +229,7 @@ contract
         <div className='ourVision '>
             <div className='h-[500px] flex justify-center'>
                 <div className='banner flex items-center justify-center'>
-                        <div className='frame bg-white pt-14 pb-14 flex items-center px-8'>
+                        <div className='frame bg-white pt-14 pb-14 flex items-center shadow-xl px-8'>
                             <div className='px-8'>
 
                             <h3 className=' text-3xl'>Beautiful Landing Page with <br />Elements</h3>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,13 @@ import {
 
 export const Navbar = () => {
   return (
-    <nav className="flex justify-evenly items-center h-16 z-20">
-        <img src={imagen} alt="" srcset="" width="60px" height="60px"/>
+    <nav className="flex justify-evenly items-center h-[92px] z-20">
+        <img src={imagen} alt="" srcset="" width="92px" height="60px"/>
         <ul className="flex font-bold text-white max-sm:flex-col max-sm:absolute max-sm:mtz-50 items-center">
-            <li className="ml-2"><Link to="/">Home</Link></li>
-            <li className="ml-2"><Link to="/about">About us</Link></li>
-            <li className="ml-2"><Link to="">Pages</Link></li>
-            <li className="ml-2"><Link to="/contact">Contact</Link></li>
+            <li className="ml-10"><Link to="/">Home</Link></li>
+            <li className="ml-10"><Link to="/about">About us</Link></li>
+            <li className="ml-10"><Link to="">Pages</Link></li>
+            <li className="ml-10"><Link to="/contact">Contact</Link></li>
         </ul>
    </nav>
   )
